170f46b86ce58b6ea82a31b48f99acb48a30fc86
Aleksey <_@example.com>
2026-07-17 15:41:58 +0300

    3

--- src/main.tf
+++ src/main.tf
@@ -8,17 +8,25 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+resource "yandex_vpc_subnet" "develop_db" {
+  name           = "${var.vpc_name}-db"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = ["10.0.2.0/24"]
+}
+
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
+
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v2"
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform_id
   resources {
-    cores         = 2
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vm_web_cores
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
   }
   boot_disk {
     initialize_params {
@@ -26,16 +34,51 @@ resource "yandex_compute_instance" "platform" {
     }
   }
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    nat       = var.vm_web_nat
   }
 
   metadata = {
-    serial-port-enable = "1"
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+    serial-port-enable = var.vm_web_serial_port_enable
+    ssh-keys           = "${var.vm_web_ssh_user}:${var.vms_ssh_root_key}"
+  }
+
+}
+
+# db
+
+resource "yandex_compute_instance" "db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  zone = var.vm_db_zone
+
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_db.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = {
+    serial-port-enable = var.vm_db_serial_port_enable
+    ssh-keys           = "${var.vm_db_ssh_user}:${var.vms_ssh_root_key}"
   }
 
 }
---
+++ src/vms_platform.tf
@@ -0,0 +1,128 @@
+# Переменные для первой ВМ (web)
+variable "vm_web_name" {
+  description = "Name VM"
+  type        = string
+  default     = "netology-develop-platform-web"
+}
+
+variable "vm_web_platform_id" {
+  description = "Platform ID"
+  type        = string
+  default     = "standard-v2"
+}
+
+variable "vm_web_cores" {
+  description = "Number of CPU"
+  type        = number
+  default     = 2
+}
+
+variable "vm_web_memory" {
+  description = "RAM GB"
+  type        = number
+  default     = 1
+}
+
+variable "vm_web_core_fraction" {
+  description = "CPU core"
+  type        = number
+  default     = 5
+}
+
+variable "vm_web_preemptible" {
+  description = ""
+  type        = bool
+  default     = true
+}
+
+variable "vm_web_nat" {
+  description = "Enable NAT on network interface"
+  type        = bool
+  default     = true
+}
+
+variable "vm_web_serial_port_enable" {
+  description = "Enable serial console (1 = enabled, 0 = disable)"
+  type        = string
+  default     = "1"
+}
+
+variable "vm_web_ssh_user" {
+  description = "Default SSH user"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "vm_web_image_family" {
+  description = "Image family"
+  type        = string
+  default     = "ubuntu-2004-lts"
+}
+
+
+# Переменные для второй ВМ (db)
+variable "vm_db_name" {
+  description = "Name of the DB VM"
+  type        = string
+  default     = "netology-develop-platform-db"
+}
+
+variable "vm_db_platform_id" {
+  description = "Platform ID for the DB VM"
+  type        = string
+  default     = "standard-v2"
+}
+
+variable "vm_db_cores" {
+  description = "CPU cores for DB VM"
+  type        = number
+  default     = 2
+}
+
+variable "vm_db_memory" {
+  description = "RAM size GB"
+  type        = number
+  default     = 2
+}
+
+variable "vm_db_core_fraction" {
+  description = "Core fraction for DB VM"
+  type        = number
+  default     = 20
+}
+
+variable "vm_db_preemptible" {
+  description = ""
+  type        = bool
+  default     = true
+}
+
+variable "vm_db_nat" {
+  description = "Enable NAT for DB VM"
+  type        = bool
+  default     = true
+}
+
+variable "vm_db_serial_port_enable" {
+  description = "Serial port enable flag"
+  type        = string
+  default     = "1"
+}
+
+variable "vm_db_ssh_user" {
+  description = "Default SSH user"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "vm_db_image_family" {
+  description = "Image family"
+  type        = string
+  default     = "ubuntu-2004-lts"
+}
+
+variable "vm_db_zone" {
+  description = "zone b"
+  type        = string
+  default     = "ru-central1-b"
+}
