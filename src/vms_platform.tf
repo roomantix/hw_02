61d7f2838c0e3ab2e32f3b9d7d8e946188cdeb60
Aleksey <_@example.com>
2026-07-17 17:01:43 +0300

    6

--- src/vms_platform.tf
+++ src/vms_platform.tf
@@ -6,23 +6,23 @@ variable "vm_web_platform_id" {
   default     = "standard-v2"
 }
 
-variable "vm_web_cores" {
-  description = "Number of CPU"
-  type        = number
-  default     = 2
-}
+#variable "vm_web_cores" {
+#  description = "Number of CPU"
+#  type        = number
+#  default     = 2
+#}
 
-variable "vm_web_memory" {
-  description = "RAM GB"
-  type        = number
-  default     = 1
-}
+#variable "vm_web_memory" {
+#  description = "RAM GB"
+#  type        = number
+#  default     = 1
+#}
 
-variable "vm_web_core_fraction" {
-  description = "CPU core"
-  type        = number
-  default     = 5
-}
+#variable "vm_web_core_fraction" {
+#  description = "CPU core"
+#  type        = number
+#  default     = 5
+#}
 
 variable "vm_web_preemptible" {
   description = ""
@@ -36,17 +36,17 @@ variable "vm_web_nat" {
   default     = true
 }
 
-variable "vm_web_serial_port_enable" {
-  description = "Enable serial console (1 = enabled, 0 = disable)"
-  type        = string
-  default     = "1"
-}
+#variable "vm_web_serial_port_enable" {
+#  description = "Enable serial console (1 = enabled, 0 = disable)"
+#  type        = string
+#  default     = "1"
+#}
 
-variable "vm_web_ssh_user" {
-  description = "Default SSH user"
-  type        = string
-  default     = "ubuntu"
-}
+#variable "vm_web_ssh_user" {
+#  description = "Default SSH user"
+#  type        = string
+#  default     = "ubuntu"
+#}
 
 variable "vm_web_image_family" {
   description = "Image family"
@@ -62,7 +62,7 @@ variable "vm_db_platform_id" {
   type        = string
   default     = "standard-v2"
 }
-
+/*
 variable "vm_db_cores" {
   description = "CPU cores for DB VM"
   type        = number
@@ -80,7 +80,7 @@ variable "vm_db_core_fraction" {
   type        = number
   default     = 20
 }
-
+*/
 variable "vm_db_preemptible" {
   description = ""
   type        = bool
@@ -92,7 +92,7 @@ variable "vm_db_nat" {
   type        = bool
   default     = true
 }
-
+/*
 variable "vm_db_serial_port_enable" {
   description = "Serial port enable flag"
   type        = string
@@ -104,7 +104,7 @@ variable "vm_db_ssh_user" {
   type        = string
   default     = "ubuntu"
 }
-
+*/
 variable "vm_db_image_family" {
   description = "Image family"
   type        = string
@@ -141,4 +141,38 @@ variable "vm_db_role" {
   description = "Role suffix for DB instance"
   type        = string
   default     = "platform-db"
+}
+
+
+variable "vms_resources" {
+  description = "Resource configurations web , db VMs"
+  type = map(object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+    hdd_size      = number
+    hdd_type      = string
+  }))
+
+  default = {
+    web = {
+      cores         = 2
+      memory        = 1
+      core_fraction = 5
+      hdd_size      = 10
+      hdd_type      = "network-hdd"
+    }
+    db = {
+      cores         = 2
+      memory        = 2
+      core_fraction = 20
+      hdd_size      = 10
+      hdd_type      = "network-hdd"
+    }
+  }
+}
+
+variable "vms_metadata" {
+  description = "Common metadata for all VMs"
+  type        = map(string)
 }
