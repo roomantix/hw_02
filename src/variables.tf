c4e0416e78ad47b1fb54439415c3b047ac4229d2
Aleksey <_@example.com>
2026-07-17 14:43:49 +0300

    1

---
+++ src/variables.tf
@@ -0,0 +1,45 @@
+###cloud vars
+
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBPJM02oeS8YDupm0QWiurQyOtUB+K8dwFwlHB0wsZ9c root@alex-VirtualBox"
+  description = "ssh-keygen -t ed25519"
+}
+
+#### service account yandex
+
+variable "service_account_key_file" {
+  description = "Path to service account key file"
+  type        = string
+}
